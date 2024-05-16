04e507310516928e65f25f2d87933086c6ba2790
Leonardo Held <leonardo.held@toradex.com>
2024-05-16 10:06:10 -0300

    Send a message to Slack when a new release is out
    Signed-off-by: Leonardo Held <leonardo.held@toradex.com>

---
+++ ci-scripts/release/send-slack-message.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <channel_id> <slack_token> <message>"
+    exit 1
+fi
+
+channel_id="$1"
+slack_token="$2"
+message="$3"
+
+curl -X POST \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $slack_token" \
+-d "{\"channel\": \"$channel_id\", \"text\": \"$message\"}" \
+https://slack.com/api/chat.postMessage
+
