f45606f93e855b20cee8e80844b2ca0d8480205d
Leonardo Held <leonardo.held@toradex.com>
2024-05-20 13:44:58 -0300

    Use the dotnet versions from yaml as build args as well
    Signed-off-by: Leonardo Held <leonardo.held@toradex.com>

---
+++ ci-scripts/build/export-dotnet-build-args.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x
+
+yaml_file="$1"
+key="$2"
+
+export DOTNET_RELEASE=$(yq e "explode(.).$key.release" "$yaml_file")
+export DOTNET_SEMVER=$(yq e "explode(.).$key.semver" "$yaml_file")
+export DOTNET_DEBUGGER_VER=$(yq e "explode(.).$key.debugger-version" "$yaml_file")
