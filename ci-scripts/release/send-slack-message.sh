a5059f03233aed2115f6734a474afb448cd9c1b3
Leonardo Held <leonardo.held@toradex.com>
2024-06-06 14:19:00 -0300

    Run shfmt through ci-scripts
    Related-to: TCCP-751
    Fix: run shfmt on shell scripts inside the ci-scripts folder
    
    Signed-off-by: Leonardo Held <leonardo.held@toradex.com>

--- ci-scripts/release/send-slack-message.sh
+++ ci-scripts/release/send-slack-message.sh
@@ -4,8 +4,8 @@
 # SPDX-License-Identifier: MIT
 
 if [ $# -ne 3 ]; then
-    echo "Usage: $0 <channel_id> <slack_token> <message>"
-    exit 1
+  echo "Usage: $0 <channel_id> <slack_token> <message>"
+  exit 1
 fi
 
 channel_id="$1"
@@ -13,8 +13,7 @@ slack_token="$2"
 message="$3"
 
 curl -X POST \
--H "Content-Type: application/json" \
--H "Authorization: Bearer $slack_token" \
--d "{\"channel\": \"$channel_id\", \"text\": \"$message\"}" \
-https://slack.com/api/chat.postMessage
-
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $slack_token" \
+  -d "{\"channel\": \"$channel_id\", \"text\": \"$message\"}" \
+  https://slack.com/api/chat.postMessage
