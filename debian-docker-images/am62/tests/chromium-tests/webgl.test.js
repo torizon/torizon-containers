e3b2292cc7f42ea77022dbc59690cb27a9e5558e
Carlos Henrique Lima Melara <carlos.melara@toradex.com>
2024-08-28 10:00:05 -0300

    am62: chromium-tests: update expected output from webglreport
    Make it match the output for am62.
    
    Related-to: TCCP-814
    Signed-off-by: Carlos Henrique Lima Melara <carlos.melara@toradex.com>
    Fix: am62: chromium-tests: update expected output from webglreport

--- debian-docker-images/am62/tests/chromium-tests/webgl.test.js
+++ debian-docker-images/am62/tests/chromium-tests/webgl.test.js
@@ -2,5 +2,7 @@ import { get_tidy_main_webgl_report } from './index.js'
 
 test('Open and parse webgl test page', async () => {
     const report = await get_tidy_main_webgl_report();
-    expect(report['Platform']).toContain('MacIntel');
+    expect(report['Context Name']).toContain('webgl2');
+    expect(report['GL Version']).toContain('WebGL 2.0 (OpenGL ES 3.0 Chromium)');
+    expect(report['Unmasked Renderer']).toContain('ANGLE (Imagination Technologies, PowerVR A-Series AXE-1-16M, OpenGL ES 3.1)');
 }, 100000);
