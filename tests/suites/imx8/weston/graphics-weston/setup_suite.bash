9319f597478a956faa98b49bc034213688c579c8
Leonardo Held <leonardo.held@toradex.com>
2024-09-03 11:11:06 -0300

    tests: imx8: graphics: force connector on
    Signed-off-by: Leonardo Held <leonardo.held@toradex.com>

--- tests/suites/imx8/weston/graphics-weston/setup_suite.bash
+++ tests/suites/imx8/weston/graphics-weston/setup_suite.bash
@@ -2,6 +2,12 @@ setup_suite() {
     docker container stop weston || true
     docker container rm weston || true
 
+    for dir in /sys/class/drm/card*-HDMI-*; do
+        if [[ -d $dir ]]; then
+            echo "on" > "$dir/status"
+        fi
+    done
+
     docker container run -d --name=weston --net=host \
     --cap-add CAP_SYS_TTY_CONFIG -v /dev:/dev -v /tmp:/tmp \
     -v /run/udev/:/run/udev/ --device-cgroup-rule="c 4:* rmw" \
@@ -30,4 +36,10 @@ teardown_suite() {
     docker container stop graphics-tests
     docker image rm -f $(docker container inspect -f '{{.Image}}' graphics-tests)
     docker container rm graphics-tests
+
+    for dir in /sys/class/drm/card*-HDMI-*; do
+        if [[ -d $dir ]]; then
+            echo "off" > "$dir/status"
+        fi
+    done
 }
