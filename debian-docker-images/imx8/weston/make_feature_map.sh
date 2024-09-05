ae479bdf96629dc62b06f9636cb73bae2347f65a
Leonardo Held <leonardo.held@toradex.com>
2024-09-05 10:49:25 -0300

    Update make_feature_map to Scarthgap
    Signed-off-by: Leonardo Held <leonardo.held@toradex.com>

--- debian-docker-images/imx8/weston/make_feature_map.sh
+++ debian-docker-images/imx8/weston/make_feature_map.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-# Fetches from the meta-freescale sources and
-# creates subdirectory 'imx_features' containing regexp maps
-# for aiding runtime decision about supported features based on SoC IDs.
+# Fetches from the meta-freescale sources and creates subdirectory
+#'imx_features' containing regexp maps for aiding runtime decision about
+# supported features based on SoC IDs.
 
 # In POSIX sh, set option pipefail is undefined, but dash and ash will support it.
 # See https://github.com/koalaman/shellcheck/issues/2555
@@ -20,7 +20,7 @@ cleanup() {
 trap cleanup EXIT
 
 echo "$WHOAMI: fetching from meta-freescale sources..." >&2
-wget -qO - 'https://raw.githubusercontent.com/Freescale/meta-freescale/dunfell/conf/machine/include/imx-base.inc' >"$IMX_BASE_INC"
+wget -qO - 'https://raw.githubusercontent.com/Freescale/meta-freescale/scarthgap/conf/machine/include/imx-base.inc' >"$IMX_BASE_INC"
 
 MAP_DIR=${WHEREAMI}/imx_features
 rm -rf "$MAP_DIR"
@@ -31,10 +31,9 @@ map_feature() {
   MAP_FILE=$MAP_DIR/${FEATURE}.socs
   echo "$WHOAMI: SoCs flagged with '$FEATURE':" >&2
   sed -r \
-    -e '/^MACHINEOVERRIDES_EXTENDER_mx[^[:blank:]]+ *=/!d' \
+    -e '/^MACHINEOVERRIDES_EXTENDER:[^[:blank:]]+ *=/!d' \
     -e '/\<'"$FEATURE"'\>/!d' \
-    -e 's/ *=.*//' \
-    -e 's/.*_//' \
+    -e 's/^[^:]+:([^:]+):.*/\1/' \
     -e 'y/abcdefghijklmnopqrstuxwxyz/ABCDEFGHIJKLMNOPQRSTUXWXYZ/' \
     -e 's/^/^i\\./' \
     <"$IMX_BASE_INC" |
