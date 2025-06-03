1924053ebfdec79675317a19fd320f3837b7027b
Leonardo Held <leonardo.held@toradex.com>
2025-06-03 09:28:43 -0300

    tests: fix qt6-enterprise
    Signed-off-by: Leonardo Held <leonardo.held@toradex.com>

--- tests/cases/qt6-enterprise-helper.sh
+++ tests/cases/qt6-enterprise-helper.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
 setup_qt6_enterprise() {
-  # For demo containers, we need a special setup
-  QT6_ENTERPRISE_DEMO_RUN_IMX8="docker container run -d -it --net=host --name=qt6-enterprise-demo \
-    --cap-add CAP_SYS_TTY_CONFIG -v /dev:/dev -v /tmp:/tmp -v /run/udev/:/run/udev/ \
-    --device-cgroup-rule='c 4:* rmw'  --device-cgroup-rule='c 13:* rmw' \
-    --device-cgroup-rule='c 226:* rmw' --device-cgroup-rule='c 29:* rmw' --device-cgroup-rule='c 199:* rmw' \
-    $REGISTRY/torizon/qt6-enterprise-demo-imx8:stable-rc bash"
-
   docker container kill qt6-enterprise || true
   docker container rm qt6-enterprise || true
   docker container kill qt6-enterprise-demo || true
@@ -15,9 +8,7 @@ setup_qt6_enterprise() {
 
   local DOCKER_RUN
   if [[ "$PLATFORM_FILTER" == *imx8* ]]; then
-    DOCKER_RUN="$QT6_ENTERPRISE_DEMO_RUN_IMX8"
-  else
-    DOCKER_RUN=$(read-docker-run.sh "/runs/qt6-enterprise/qt6-enterprise-upstream-compose.run" "qt6-enterprise" "qt6-enterprise")
+    DOCKER_RUN=$(read-docker-run.sh "/runs/qt6-enterprise/qt6-enterprise-imx8-compose.run" "qt6-enterprise-imx8" "qt6-enterprise")
   fi
 
   eval "$DOCKER_RUN"
