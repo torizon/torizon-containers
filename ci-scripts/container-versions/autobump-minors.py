5905920e9028aa87c6478214af3a04c62fe64d2d
Leonardo Held <leonardo.held@toradex.com>
2025-06-11 15:13:49 -0300

    container-versions: use uv for the helper script
    Signed-off-by: Leonardo Held <leonardo.held@toradex.com>

--- ci-scripts/container-versions/autobump-minors.py
+++ ci-scripts/container-versions/autobump-minors.py
@@ -1,12 +1,16 @@
+#!/usr/bin/env -S uv run
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "ruamel-yaml",
+# ]
+# ///
 # This is a small script that may be useful during bump releases, where we
 # by definition bump all the minors in the respective yml files in the
 # container-versions dir.
 
-# Before running:
-# python3 -m venv venv
-# pip install ruamel.yaml
-
-# Then run as `python autobum-minors.py .``
+# You need `uv` installed to run this program https://docs.astral.sh/uv/
+# Then run as `./autobump-minors.py .`
 
 import os
 import sys
