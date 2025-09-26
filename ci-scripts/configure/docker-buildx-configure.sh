4754f06531836403192f27933ba9695b8898401c
Leonardo Held <leonardo.held@toradex.com>
2025-09-26 15:18:04 -0300

    build: drop artifactory for now due to network issues with legacy runners
    Related-to: TCCP-1090
    
    Signed-off-by: Leonardo Held <leonardo.held@toradex.com>

--- ci-scripts/configure/docker-buildx-configure.sh
+++ ci-scripts/configure/docker-buildx-configure.sh
@@ -10,7 +10,7 @@ docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD" "$CI_REGISTRY"
 
 docker info
 
-docker buildx create --config "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"/buildkitd.toml --name multiarch-builder --driver docker-container --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --use
+docker buildx create --name multiarch-builder --driver docker-container --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --use
 docker buildx inspect --bootstrap
 
 docker run --privileged --rm "${TORADEX_INTERNAL_DOCKERHUB_CACHE}/tonistiigi/binfmt" --install arm64,arm
