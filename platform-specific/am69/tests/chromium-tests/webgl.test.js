ad0ebda0f2510740f9efc56925a2e0ff18c43730
Flavio Ipiranga <flavio.araujo@toradex.com>
2025-10-01 10:20:38 -0300

    am69: fix chromium webgl test to check for correct version
    The am69 webgl test should be similar to imx95 webgl test.
    The reason is they use different WebGL versions from the current
    SOCs.
    
    Relates-to:TCCP-1102
    
    Signed-off-by: Flavio Ipiranga <flavio.araujo@toradex.com>

--- platform-specific/am69/tests/chromium-tests/webgl.test.js
+++ platform-specific/am69/tests/chromium-tests/webgl.test.js
@@ -2,7 +2,8 @@ import { get_tidy_main_webgl_report } from './index.js'
 
 test('Open and parse webgl test page', async () => {
     const report = await get_tidy_main_webgl_report();
-    expect(report['Context Name']).toContain('webgl');
+    expect(report['Context Name']).toContain('webgl2');
+    expect(report['GL Version']).toContain('WebGL 2.0 (OpenGL ES 3.0 Chromium)');
     // matches any substring "PowerVR"
     expect(report['Unmasked Renderer'][0]).toEqual(expect.stringContaining('PowerVR'));
 }, 100000);
