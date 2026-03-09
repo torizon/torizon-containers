fa1fd9906e77668773e20530ba7e22fa1209c192
Julio Nicolucci <julio.nicolucci@toradex.com>
2026-03-09 15:42:49 -0300

    demo-gallery: add pdf-reader remaining platforms
    The platforms are:
    - AM62
    - AM62P
    - AM69
    - iMX95
    
    Related-to: DWC-8620
    
    Signed-off-by: Julio Nicolucci <julio.nicolucci@toradex.com>

--- tests/cases/pdf-reader-helper.sh
+++ tests/cases/pdf-reader-helper.sh
@@ -7,7 +7,16 @@ setup_pdf-reader() {
   local DOCKER_RUN
   if [[ "$PLATFORM_FILTER" == *imx8* ]]; then
     DOCKER_RUN=$(read-docker-run.sh "/runs/pdf-reader/pdf-reader-imx8-compose.run" "pdf-reader-imx8" "pdf-reader")
+  elif [[ "$PLATFORM_FILTER" == *imx95* ]]; then
+    DOCKER_RUN=$(read-docker-run.sh "/runs/pdf-reader/pdf-reader-imx95-compose.run" "pdf-reader-imx95" "pdf-reader")
+  elif [[ "$PLATFORM_FILTER" == *am62* ]]; then
+    DOCKER_RUN=$(read-docker-run.sh "/runs/pdf-reader/pdf-reader-am62-compose.run" "pdf-reader-am62" "pdf-reader")
+  elif [[ "$PLATFORM_FILTER" == *am62p* ]]; then
+    DOCKER_RUN=$(read-docker-run.sh "/runs/pdf-reader/pdf-reader-am62p-compose.run" "pdf-reader-am62p" "pdf-reader")
+  elif [[ "$PLATFORM_FILTER" == *am69* ]]; then
+    DOCKER_RUN=$(read-docker-run.sh "/runs/pdf-reader/pdf-reader-am69-compose.run" "pdf-reader-am69" "pdf-reader")
   else
+    # return 1, since there is no PDF reader image for upstream, and we want to skip the test instead of failing it
     return 1
   fi
 
