5f73b6210e9d14677f18d8980e0d5e0ae03488ff
Leonardo Bornia <leonardo.bornia@toradex.com>
2026-09-02 10:31:31 -0300

    weston: make_feature_map: Update for wrynose
    Signed-off-by: Leonardo Bornia <leonardo.bornia@toradex.com>

--- support-files/weston/make_feature_map.sh
+++ support-files/weston/make_feature_map.sh
@@ -20,7 +20,7 @@ cleanup() {
 trap cleanup EXIT
 
 echo "$WHOAMI: fetching from meta-freescale sources..." >&2
-wget -qO - 'https://raw.githubusercontent.com/Freescale/meta-freescale/scarthgap/conf/machine/include/imx-base.inc' >"$IMX_BASE_INC"
+wget -qO - 'https://raw.githubusercontent.com/Freescale/meta-freescale/wrynose/conf/machine/include/imx-base.inc' >"$IMX_BASE_INC"
 
 MAP_DIR=${WHEREAMI}/imx_features
 rm -rf "$MAP_DIR"
